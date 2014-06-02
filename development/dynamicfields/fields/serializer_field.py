7cffd394498eea75a8d2fb973de7fd3d8674b463
twstddev <twstd.dev@gmail.com>
2014-06-02 22:44:59 +0000

    Add function to the base field that converts database value to a dictionary

--- development/dynamicfields/fields/serializer_field.py
+++ development/dynamicfields/fields/serializer_field.py
@@ -8,8 +8,29 @@ class SerializerField( models.Field ):
 	"""
 	__metaclass__ = models.SubfieldBase
 
+	def __init__( self, *args, **kwargs ):
+		"""
+		Preset default and blank properties.
+
+		It is done to avoid extra work on field
+		initialization in a model.
+		"""
+		kwargs[ "default" ] = ""
+		kwargs[ "blank" ] = True
+		super( SerializerField, self ).__init__( *args, **kwargs )
+
 	def db_type( self, connection ):
 		"""
 		Tells django what is the type of this field.
 		"""
 		return "text"
+
+	def to_python( self, value ):
+		"""
+		Converts the received value from a database
+		to a dictionary.
+		"""
+		if isinstance( value, basestring ):
+			return json.load( value )
+
+		return value
