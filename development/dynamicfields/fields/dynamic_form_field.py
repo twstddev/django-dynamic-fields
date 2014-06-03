64b27bb47857b935a190911b83dfd19cec48398f
twstddev <twstd.dev@gmail.com>
2014-06-03 15:33:04 +0000

    Add test for form field

--- development/dynamicfields/fields/dynamic_form_field.py
+++ development/dynamicfields/fields/dynamic_form_field.py
@@ -1,4 +1,19 @@
 from django import forms
 
-class DynamicFormField( forms.Field ):
-	pass
+class DynamicFormField( forms.MultiValueField ):
+	"""
+	Implements a form field that contains a list
+	of fields and nested forms as well as arrays.
+	"""
+	def __init__( self, form, **kwargs ):
+		self.form = form()
+
+		self.fields = [ field for field in self.form ]
+
+		super( DynamicFormField, self ).__init__( **kwargs )
+
+	def compress( self, values ):
+		"""
+		Return a string represented or "compressed" value.
+		"""
+		pass
--- development/dynamicfields/tests/fields/dynamic_form_field_spec.py
+++ development/dynamicfields/tests/fields/dynamic_form_field_spec.py
@@ -1,1 +1,51 @@
+from django.test import TestCase
+from django import forms
+from dynamicfields.fields import DynamicFormField
 
+class CustomForm( forms.Form ):
+	first = forms.CharField( max_length = 255 )
+	last = forms.CharField( max_length = 255 )
+
+class ParentForm( forms.Form ):
+	field_here = forms.CharField( max_length = 255 )
+	nested_form = DynamicFormField( form = CustomForm )
+
+class DynamicFormFieldTestCase( TestCase ):
+	"""
+	Test main form field.
+	"""
+	def setUp( self ):
+		self.m_data = {
+			'first' : 'name',
+			'last' : 'surname',
+		}
+
+		self.m_nested_data = {
+			"field_here" : "some_value",
+			"nested_form" : self.m_data,
+		}
+
+	def test_returns_set_values( self ):
+		"""
+		Makes sure that field returns supplied list
+		of values.
+		"""
+		field = DynamicFormField( CustomForm, initial = self.m_data )
+		self.assertEqual( field.clean( [ "name", "surname" ] ), self.m_data )
+
+	def test_allows_nested_form_fields( self ):
+		"""
+		Makes sure that nested values are returned.
+		"""
+		field = DynamicFormField( ParentForm, initial = self.m_nested_data );
+		self.assertEqual( field.clean( [ "somve_value", [ "name", "surname" ] ] ),  self.m_nested_data )
+
+	def test_validates_nested_fields( self ):
+		"""
+		Makes sure that any nested field validates.
+		"""
+		form = ParentForm();
+		self.assertFalse( form.is_valid() )
+
+		form = ParentForm( self.m_nested_data )
+		self.assertTrue( form.is_valid() )
